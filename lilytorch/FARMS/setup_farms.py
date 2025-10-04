9e44cb796795d39245565ef1c0d25a35802d7fa1
Jonathan Arreguit <jarreguit@gmail.com>
2025-10-04 02:21:48 +0200

    [FARMS] Update to FARMS v0.2

--- lilytorch/FARMS/setup_farms.py
+++ lilytorch/FARMS/setup_farms.py
@@ -1,42 +1,58 @@
-"""Clone all FARMS repos"""
+"""Clone FARMS repos"""
 
 import os
 import sys
 from subprocess import check_call
 try:
-    from git import Repo
+    import uv
 except ImportError:
-    check_call([sys.executable, '-m', 'pip', 'install', 'GitPython'])
-    from git import Repo
+    check_call([sys.executable, '-m', 'pip', 'install', 'uv'])
+try:
+    from git import Repo, GitCommandError
+except ImportError:
+    print('Installing GitPython')
+    check_call(['uv', 'pip', 'install', 'GitPython'])
+    from git import Repo, GitCommandError
 
-def pip_install(
-    package_name: str,
-    local_package: bool = False,
-):
-    ''' Install a package using pip '''
-    print(f'Installing {package_name}')
-    pip_install = ['pip', 'install']
-    if local_package:
-        pip_install.append('-e')
-    check_call(pip_install + [package_name])
-    print(f'Completed installation of {package_name}\n')
-    return
 
 def main():
     """Main"""
+    pip_install = ['uv', 'pip', 'install', '--no-build-isolation']
+    for package, branch, install in [
+            ['farms_core', 'amphibious_v0.2', True],
+            ['farms_mujoco', 'amphibious_v0.2', True],
+            ['farms_sim', 'amphibious_v0.2', True],
+            ['farms_amphibious', 'amphibious_v0.2', True],
+    ]:
+        print(f'Setting up {package}')
+        if not os.path.isdir(package):
+            repo = Repo.clone_from(
+                f'git@github.com:farmsim/{package}.git',
+                package,
+                branch=branch,
+            )
+        repo = Repo(package)
+        current_branch = repo.active_branch.name
+        if current_branch != branch:
+            print(f'Checking out branch {branch} for {package}')
+            try:
+                repo.git.checkout(branch)
+            except GitCommandError:
+                print(f'Branch {branch} not found locally, trying to fetch it from origin')
+                repo.remotes.origin.fetch()
+                repo.git.checkout(branch)
+        print(f'Active branch of {package}: {repo.active_branch.name}')
+        # print(f'Pulling latest version of {package}')
+        # repo.remotes.origin.pull()
+        if install:
+            requirements = f'{package}/requirements.txt'
+            if os.path.isfile(requirements):
+                print(f'Installing {package} dependencies')
+                check_call(pip_install + ['-r', requirements])
+            print(f'Installing {package}')
+            check_call(pip_install + ['-e', package, '-v'])  # vvv
+        print(f'Completed setup for {package}\n')
 
-    # Install MuJoCo
-    pip_install('mujoco')
-
-    # Install dm_control
-    pip_install('dm_control')
-
-    # Farms pylog
-    pip_install('git+https://gitlab.com/FARMSIM/farms_pylog.git')
-
-    # FARMS
-    for package in ['farms_core', 'farms_mujoco', 'farms_sim', 'farms_amphibious']:
-        pip_install(package, local_package=True)
 
 if __name__ == '__main__':
     main()
